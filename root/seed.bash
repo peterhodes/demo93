fecf9f87ba3aa1d8e413c333315e418dcc76250b
Peter Rhodes <33201786+peterhodes@users.noreply.github.com>
2022-11-14 18:05:18 +0000

    Update seed.bash

--- root/seed.bash
+++ root/seed.bash
@@ -14,12 +14,22 @@ unzip terraform*.zip
 cp terraform /usr/local/bin
 cd -
 
+cd /root
+
 mkdir /root/terraform
-curl -o /root/terraform/main.tf          https://raw.githubusercontent.com/peterhodes/demo93/main/root/terraform/main.tf
+mkdir /root/vpcquery
+
 curl -o /root/doit.bash                  https://raw.githubusercontent.com/peterhodes/demo93/main/root/doit.bash
+curl -o /root/doit-peering.py            https://raw.githubusercontent.com/peterhodes/demo93/main/root/doit-peering.py
 curl -o /root/get_hosts                  https://raw.githubusercontent.com/peterhodes/demo93/main/root/get_hosts
 curl -o /root/get_vpcs                   https://raw.githubusercontent.com/peterhodes/demo93/main/root/get_vpcs
+curl -o /root/get_vpcs.py                https://raw.githubusercontent.com/peterhodes/demo93/main/root/get_vpcs.py
 curl -o /root/archived_scripts           https://raw.githubusercontent.com/peterhodes/demo93/main/root/archived_scripts
 
-cd /root
-chmod +x doit.bash get_hosts get_vpcs
+chmod +x doit.bash doit-peering.py get_hosts get_vpcs get_vpcs.py
+
+curl -o /root/terraform/main.tf          https://raw.githubusercontent.com/peterhodes/demo93/main/root/terraform/main.tf
+curl -o /root/vpcquery/json1             https://raw.githubusercontent.com/peterhodes/demo93/main/root/vpcquery/json1
+curl -o /root/vpcquery/json2             https://raw.githubusercontent.com/peterhodes/demo93/main/root/vpcquery/json2
+curl -o /root/vpcquery/json93            https://raw.githubusercontent.com/peterhodes/demo93/main/root/vpcquery/json93
+curl -o /root/vpcquery/l                 https://raw.githubusercontent.com/peterhodes/demo93/main/root/vpcquery/l
